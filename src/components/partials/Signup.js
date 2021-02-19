a43e369191be1fbac9f69aa3f5cb04519819eef3
wkoslik <wkoslik@gmail.com>
2021-02-19 14:41:36 -0500

    sign up post route

--- src/components/partials/Signup.js
+++ src/components/partials/Signup.js
@@ -1,16 +1,29 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 const Signup = (props) => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('');
-//TODO add form password verification
+    //TODO add form password verification
 
-const handleSubmit = (e) =>{
-    e.preventDefault()
+    const handleSubmit = (e) => {
+        e.preventDefault()
 
-    console.log('Sending up user data', {name, email, password})
-}
+        axios.post(`${process.env.REACT_APP_SERVER_URL}/api/signup`,
+            { name, email, password }
+        )
+            .then(response => {
+                console.log(response.data)
+                //save the token in local storage
+                localStorage.setItem('jwtToken', response.data.token)
+                //set the token to auth header
+                //set user data
+            })
+            .catch(err => console.log(`💩 Uh oh: \n`, err))
+        // axios.get(process.env.REACT_APP_SERVER_URL)
+        // .then(response => console.log(response.data))
+    }
 
     return (
         <section>
@@ -18,15 +31,15 @@ const handleSubmit = (e) =>{
             <form onSubmit={handleSubmit}>
                 <div className="form-elem">
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" placeholder="Display name" onChange={ e => setName(e.target.value)} />
+                    <input type="text" name="name" placeholder="Display name" onChange={e => setName(e.target.value)} />
                 </div>
                 <div className="form-elem">
                     <label htmlFor="email">Email: </label>
-                    <input type="email" name="Email" placeholder="Your Email" onChange={ e => setEmail(e.target.value)} />
+                    <input type="email" name="Email" placeholder="Your Email" onChange={e => setEmail(e.target.value)} />
                 </div>
                 <div className="form-elem">
                     <label htmlFor="password">Password: </label>
-                    <input type="password" name="password" placeholder="Your password" onChange={ e => setPassword(e.target.value)} />
+                    <input type="password" name="password" placeholder="Your password" onChange={e => setPassword(e.target.value)} />
                 </div>
                 <input type="submit" value="Sign up" />
             </form>
