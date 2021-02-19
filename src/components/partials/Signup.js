60e6c5d04efad4fbbdcd744c3a5292228d1e9776
wkoslik <wkoslik@gmail.com>
2021-02-19 13:57:29 -0500

    sign up page

--- src/components/partials/Signup.js
+++ src/components/partials/Signup.js
@@ -1,7 +1,35 @@
+import { useState } from 'react'
+
 const Signup = (props) => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+//TODO add form password verification
+
+const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    console.log('Sending up user data', {name, email, password})
+}
+
     return (
         <section>
-            <h2>SIGNUP</h2>
+            <h2>Sign Up</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-elem">
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" name="name" placeholder="Display name" onChange={ e => setName(e.target.value)} />
+                </div>
+                <div className="form-elem">
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="Email" placeholder="Your Email" onChange={ e => setEmail(e.target.value)} />
+                </div>
+                <div className="form-elem">
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" placeholder="Your password" onChange={ e => setPassword(e.target.value)} />
+                </div>
+                <input type="submit" value="Sign up" />
+            </form>
         </section>
     )
 }
